674ffd4c4ce994afa3913dc4c4b74f56b8fa8d9b
dltbwoddl <60048456+dltbwoddl@users.noreply.github.com>
2020-12-21 14:55:01 +0900

    1221
    1221

--- back/src/index.js
+++ back/src/index.js
@@ -9,6 +9,7 @@ const Promise = require('bluebird')
 Promise.promisifyAll(mysql);
 Promise.promisifyAll(require('mysql/lib/Connection').prototype)
 Promise.promisifyAll(require('mysql/lib/Pool').prototype);
+var _ = require('lodash');
 
 
 app.use(bodyParser.urlencoded({ extended: false }))
@@ -35,39 +36,32 @@ app.get('/:longgoal_id', (req, res) => {
     gethomedata.getmiddlegoal(req, res, longgoal_id);
 })
 
-//longgoal에서 가져와서 어디서부터 추가할지 정하기.
-//추가하기.
+//longgoal에서 받아왔을 때
+//key
 app.post('/modify', (req, res) => {
-    console.log(10000)
-    console.log(req.body)
-    var data = ``
-    const pool_1 = new pool();
-    Promise.using(pool_1.connect(), conn => {
-        conn.queryAsync('select * from longgoal').then((ret) => {
-            console.log(33)
-            console.log(Object.keys(ret).length)
-            var lenret = Object.keys(ret).length
-            for (i in req.body) {
-                if(i>lenret){
-                    data+=`(${i},${req.body[i]})`
-                }
-                console.log(req.body[i])
-            }
-            console.log(data);
-        }).then(() => {
-            console.log(data);
-            conn.queryAsync(`INSERT INTO longgoal(longgoal_id,longgoal) values ${data};`).then((ret) => {
-                console.log(ret);
-                console.log(10);
-            }).then(ret => {
-                pool_1.end();
-            })
-                .catch(err => {
-                    pool_1.end();
-                })
-        })
-    })
-})
+    console.log(req.body);
+    //{ '1': '코딩 마스터', '2': '인문학 마스터', '3': '123' }
+    console.log(100000)
+    var data = req.body;
+    for(i in data){
+        console.log(data[i])
+    }
+//     //users안에 있는 객체들이 o에 하나씩 들어가 age가 40보다 작으면 return하고 종료한다.
+//   console.log(_.findKey(users, function(o) { return o.age < 40; }));
+//   // =>bal
+   
+//   //age:35, active:true 조건에 맞는 객체를 가져온다.
+//   console.log(_.findKey(users, { 'age': 35, 'active': true }));
+//   // => story
+   
+//   //active값이 false인 객체를 가져온다.
+//   console.log(_.findKey(users, ['active', false]));
+//   // => mo
+
+//   //active가 true인 첫 번째 값을 가져와 보여준다.
+//   console.log(_.findKey(users, 'active'));
+//   // => bal
+});
 
 app.listen(port, () => {
     console.log('success')
